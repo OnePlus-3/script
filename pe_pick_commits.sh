f0c4cf56a7ab903f65398de9964330ee811180b2
Gaurav241 <44023468+Gaurav241@users.noreply.github.com>
2022-04-29 13:50:56 +0530

    script: Remove commit which already exists in PE ORG

--- pe_pick_commits.sh
+++ pe_pick_commits.sh
@@ -8,14 +8,6 @@ repo init -u https://github.com/PixelExperience/manifest -b twelve-plus
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-cd art
-	git fetch "https://github.com/LineageOS/android_art" refs/changes/97/318097/1 && git cherry-pick FETCH_HEAD
-cd ..
-
-cd packages/modules/adb/
-	git fetch "https://github.com/LineageOS/android_packages_modules_adb" refs/changes/85/326385/1 && git cherry-pick FETCH_HEAD
-cd ../../..
-
 cd system/bpf
 	git fetch "https://github.com/LineageOS/android_system_bpf" refs/changes/91/320591/1 && git cherry-pick FETCH_HEAD
 cd ../..
