2c6ee7038e50a9d9db42d99a64896a84ff4842bc
Gaurav241 <gaurav.govinda2420@gmail.com>
2022-05-04 12:12:01 +0530

    script: Rename to los

--- los_pick_commits.sh
+++ los_pick_commits.sh
@@ -16,10 +16,6 @@ cd packages/modules/adb/
 	git fetch "https://github.com/LineageOS/android_packages_modules_adb" refs/changes/85/326385/1 && git cherry-pick FETCH_HEAD
 cd ../../..
 
-cd packages/modules/NetworkStack
-	git fetch "https://github.com/DerpFest-11/packages_modules_NetworkStack" 11 && git cherry-pick 22fd53a977eeaf4e36be7bf6358ecf2c2737fa5e
-cd ../../..
-
 cd system/bpf
 	git fetch "https://github.com/LineageOS/android_system_bpf" refs/changes/91/320591/1 && git cherry-pick FETCH_HEAD
 cd ../..
